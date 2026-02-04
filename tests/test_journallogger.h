da62ddd942fdc97ff1800cc6f7dbb45abd86a3c9
Andreas Müller <schnitzeltony@gmail.com>
2026-02-04 22:48:11 +0100

    Add boilerplate for tests
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ tests/test_journallogger.h
@@ -0,0 +1,13 @@
+#ifndef TEST_JOURNALLOGGER_H
+#define TEST_JOURNALLOGGER_H
+
+#include <QObject>
+
+class test_journallogger : public QObject
+{
+    Q_OBJECT
+private slots:
+    void journalPlayground();
+};
+
+#endif // TEST_JOURNALLOGGER_H
